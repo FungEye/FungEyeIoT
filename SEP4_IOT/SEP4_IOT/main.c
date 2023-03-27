6d4b40fb831690a7fab76d8de8a4ecab68066f9a
KrunoNeric <91593987+KrunoNeric@users.noreply.github.com>
2023-03-27 09:45:31 +0200

    Blank MicrochipStudio project

---
+++ SEP4_IOT/SEP4_IOT/main.c
@@ -0,0 +1,18 @@
+/*
+ * SEP4_IOT.c
+ *
+ * Created: 27.3.2023. 9:44:37
+ * Author : Kruno
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
