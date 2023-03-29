092307856274e02c2b6e30832cde8afea954daf7
KrunoNeric <91593987+KrunoNeric@users.noreply.github.com>
2023-03-29 14:16:49 +0200

    Working recording of temerature and humidity

--- SEP4_IOT/SEP4_IOT/drivers/Example/main.c
+++ SEP4_IOT/SEP4_IOT/drivers/Example/main.c
@@ -39,16 +39,13 @@ void create_tasks_and_semaphores(void)
 {
 	// Semaphores are useful to stop a Task proceeding, where it should be paused to wait,
 	// because it is sharing a resource, such as the Serial port.
-	// Semaphores should only be used whilst the scheduler is running,
-	// but we can set it up here.
-	if ( xTestSemaphore == NULL )  // Check to confirm that 
-									//the Semaphore has not already been created.
+	// Semaphores should only be used whilst the scheduler is running, but we can set it up here.
+	if ( xTestSemaphore == NULL )  // Check to confirm that the Semaphore has not already been created.
 	{
 		xTestSemaphore = xSemaphoreCreateMutex();  // Create a mutex semaphore.
 		if ( ( xTestSemaphore ) != NULL )
 		{
-			xSemaphoreGive( ( xTestSemaphore ) );  // Make the mutex available for use,
-													// by initially "Giving" the Semaphore.
+			xSemaphoreGive( ( xTestSemaphore ) );  // Make the mutex available for use, by initially "Giving" the Semaphore.
 		}
 	}
 
@@ -72,31 +69,42 @@ void create_tasks_and_semaphores(void)
 /*-----------------------------------------------------------*/
 void task1( void *pvParameters )
 {
-	while(1)
+	TickType_t xLastWakeTime;
+	const TickType_t xFrequency = 500/portTICK_PERIOD_MS; // 500 ms
+
+	// Initialise the xLastWakeTime variable with the current time.
+	xLastWakeTime = xTaskGetTickCount();
+
+	for(;;)
 	{
+		xTaskDelayUntil( &xLastWakeTime, xFrequency );
 		printf("----Start----");
 		int16_t humidity = 0.0;
 		int16_t temp = 0.0;
 		
-		puts("Tying to wake up");	
+		puts("Tying to wake up");
 		if (HIH8120_OK != hih8120_wakeup())
 		{
-			puts("issue 1");
+			puts("WAKEUP FAILED");
+		}else
+		{
+			puts("AWAKE");
 		}
-		vTaskDelay(pdMS_TO_TICKS(60));
+		vTaskDelay(pdMS_TO_TICKS(100));
 		
 		puts("Meauring");
 		if (HIH8120_OK != hih8120_measure())
 		{
-			puts("issue 2");
+			puts("MESURING FAILED");
+		}else
+		{
+			puts("MESURING SUCCESSFUL");
 		}
-		
 		humidity = hih8120_getHumidityPercent_x10();
 		temp = hih8120_getTemperature_x10();
-		printf("%d",temp);
-		printf("%d",humidity);
+		printf("TEMP: %d\n",temp);
+		printf("HUMID: %d\n",humidity);
 		printf("----END----");
-		vTaskDelay(pdMS_TO_TICKS(1000));	
 	}
 }
 
@@ -121,12 +129,13 @@ void task2( void *pvParameters )
 void initialiseSystem()
 {
 	// Set output ports for leds used in the example
-	DDRA |= _BV(DDA4) | _BV(DDA5);
+	DDRA |= _BV(DDA0) | _BV(DDA7);
 
-	// Make it possible to use stdio on COM port 0 (USB)
-	// on Arduino board - Setting 57600,8,N,1
+	// Make it possible to use stdio on COM port 0 (USB) on Arduino board - Setting 57600,8,N,1
 	stdio_initialise(ser_USART0);
-	
+	// Let's create some tasks
+	create_tasks_and_semaphores();
+
 	// vvvvvvvvvvvvvvvvv BELOW IS LoRaWAN initialisation vvvvvvvvvvvvvvvvvvvvvvvvvvvvvv
 	// Status Leds driver
 	status_leds_initialise(5); // Priority 5 for internal task
@@ -141,31 +150,7 @@ void initialiseSystem()
 	{
 		puts("hih8120 FAILED");
 	}
-	
-}
 
-void readTempHum()
-{
-	printf("----Start----");
-	if ( HIH8120_OK == hih8120_initialise())
-	{
-		float humidity = 0.0;
-		float temp = 0.0;
-		
-		if (HIH8120_OK != hih8120_wakeup())
-		{
-			puts("issue 1");
-		}
-		
-		if (HIH8120_OK != hih8120_measure())
-		{
-			puts("issue 2");
-		}
-		humidity = hih8120_getHumidity();
-		temp = hih8120_getTemperature();
-
-		printf("----END----");
-	}
 }
 
 /*-----------------------------------------------------------*/
@@ -180,4 +165,3 @@ int main(void)
 	{
 	}
 }
-
