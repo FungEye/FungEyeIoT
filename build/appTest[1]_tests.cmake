b2e114d7b0e808cb1afa68d0b41d0b58a9c7c497
ninawrona <ninawrona@gmail.com>
2023-05-07 22:30:30 +0200

    adding empty test

--- build/appTest[1]_tests.cmake
+++ build/appTest[1]_tests.cmake
@@ -1,1 +1,3 @@
-set(  appTest_TESTS)
+add_test([=[Test_production.Test1]=]  [==[C:/Users/ninaw/OneDrive - ViaUC/Dokumenter/GitHub/FungEyeIoT/build/Debug/appTest.exe]==] [==[--gtest_filter=Test_production.Test1]==] --gtest_also_run_disabled_tests)
+set_tests_properties([=[Test_production.Test1]=]  PROPERTIES WORKING_DIRECTORY [==[C:/Users/ninaw/OneDrive - ViaUC/Dokumenter/GitHub/FungEyeIoT/build]==] SKIP_REGULAR_EXPRESSION [==[\[  SKIPPED \]]==])
+set(  appTest_TESTS Test_production.Test1)
