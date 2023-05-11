9b7fd8415acfd54e9da9f5d1154e43a8e5a00189
Raedrim <43709947+Raedrim@users.noreply.github.com>
2023-05-11 13:43:32 +0200

    Fix: Lux is working with seemingly normal results
    Everything is broken tho...
    TG-4

--- SEP4_IOT/SEP4_IOT/Main/srcFiles/Light.c
+++ SEP4_IOT/SEP4_IOT/Main/srcFiles/Light.c
@@ -16,80 +16,79 @@ uint16_t infraredRaw;
 // Callback function for TSL2591 driver
 void tsl2591Callback(tsl2591_returnCode_t rc)
 {
-		vTaskDelay(pdMS_TO_TICKS(6000));
-		switch (rc)
+	uint16_t tmp;
+
+	switch (rc)
+	{
+		case TSL2591_DATA_READY:
+		
+		if (TSL2591_OK == tsl2591_getLux(&luxValue))
 		{
-			case TSL2591_DATA_READY:
+			luxInInt = (int16_t)luxValue;
+			char luxString[16];
+			sprintf(luxString, "Lux: %d", luxInInt);
+			puts(luxString);
+			vTaskDelay(pdMS_TO_TICKS(6000));
+		}
+		break;
 
-			if (TSL2591_OK == tsl2591_getFullSpectrumRaw(&fullRaw))
-			{
-				// Full spectrum raw data available, use 'fullRaw' variable
-			}
-
-			if (TSL2591_OK == tsl259_getVisibleRaw(&visibleRaw))
-			{
-				// Visible raw data available, use 'visibleRaw' variable
-			}
-
-			if (TSL2591_OK == tsl2591_getInfraredRaw(&infraredRaw))
-			{
-				// Infrared raw data available, use 'infraredRaw' variable
-			}
-			
-
-			if (TSL2591_OK == tsl2591_getLux(&luxValue))
-			{
-				luxInInt= (int16_t)luxValue;
-				printf("Lux:%d\n", luxInInt);
-			}
+		case TSL2591_OK:
+			// Last command performed successfully
 			break;
 
-			// Handle other return codes if needed
-		}
-}
-	
-void initialize_Light(){
-	tsl2591_initialise(tsl2591Callback);
-}
+		case TSL2591_DEV_ID_READY:
+			// Dev ID now fetched
+			break;
 
-
-void lightTask_run(){
-	if ( TSL2591_OK == tsl2591_enable() )
-	{
+		default:
+			break;
 	}
-	
-	// The power up command is now send to the sensor - it can be powered down with a call to tsl2591_disable()
-		if ( TSL2591_OK != tsl2591_fetchData() )
-		{
-			// Something went wrong
-			// Investigate the return code further
-		}
-		else
-		{
-			//The light data will be ready after the driver calls the call back function with TSL2591_DATA_READY.
-		}
-	
-	//tsl2591_disable();
-
-	
 }
 
+void initialize_Light()
+{
+	if (TSL2591_OK == tsl2591_initialise(tsl2591Callback))
+	{
+		// Driver initialized successfully
+		// Always check what tsl2591_initialise() returns
+	}
+}
 
-void lightTask_create(){
+void lightTask_run()
+{
+	if (TSL2591_OK == tsl2591_enable())
+	{
+		// The power-up command is now sent to the sensor
+		// It can be powered down with a call to tsl2591_disable()
+	}
+
+	if (TSL2591_OK != tsl2591_fetchData())
+	{
+		// Something went wrong
+		// Investigate the return code further
+	}
+	else
+	{
+		// The light data will be ready after the driver calls the callback function with TSL2591_DATA_READY.
+	}
+}
+
+void lightTask_create()
+{
 	xTaskCreate(
-	_runLight,				       // Name of method
-	"Light",						   // A name just for humans
-	configMINIMAL_STACK_SIZE,	   // This stack size can be checked & adjusted by reading the Stack Highwater
-	NULL,					   	   // (void *pvParameters)
-	1,							   //the priority of the task
-	NULL							   //No TaskHandle created.
-	);
+		_runLight,				       // Name of method
+		"Light",						   // A name just for humans
+		configMINIMAL_STACK_SIZE,	   // This stack size can be checked & adjusted by reading the Stack Highwater
+		NULL,					   	   // (void *pvParameters)
+		1,							   // the priority of the task
+		NULL						  
+// No TaskHandle created.
+);
 }
 
-
-void _runLight(void* params){
-	// In future we could use this for some INIT values
-	
+void _runLight(void* params)
+{
+	// In the future, we could use this for some INIT values
 	while (1) {
 		lightTask_run();
 	}
