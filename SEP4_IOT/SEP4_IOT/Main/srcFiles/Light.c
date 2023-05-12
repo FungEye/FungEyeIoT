7931ceeb6050a96ac38e570d6e50bebed42db46c
Raedrim <43709947+Raedrim@users.noreply.github.com>
2023-05-12 09:34:10 +0200

    semaprhores fixed with light
    TG-4

--- SEP4_IOT/SEP4_IOT/Main/srcFiles/Light.c
+++ SEP4_IOT/SEP4_IOT/Main/srcFiles/Light.c
@@ -14,85 +14,85 @@ extern SemaphoreHandle_t semaphoreLight;
 // Callback function for TSL2591 driver
 void tsl2591Callback(tsl2591_returnCode_t rc)
 {
-	uint16_t tmp;
+    switch (rc)
+    {
+        case TSL2591_DATA_READY:
+            if (TSL2591_OK == tsl2591_getLux(&luxValue))
+            {
+                luxInInt = (int16_t)luxValue;
+                printf("\nLux: %d\n", luxInInt);
+            }
+            break;
 
-	switch (rc)
-	{
-		case TSL2591_DATA_READY:
-		
-		if (TSL2591_OK == tsl2591_getLux(&luxValue))
-		{
-			luxInInt = (int16_t)luxValue;
-			char luxString[16];
-			sprintf(luxString, "Lux: %d", luxInInt);
-			puts(luxString);
-			vTaskDelay(pdMS_TO_TICKS(6000));
-		}
-		break;
+        case TSL2591_OK:
+            // Last command performed successfully
+            break;
 
-		case TSL2591_OK:
-			// Last command performed successfully
-			break;
+        case TSL2591_DEV_ID_READY:
+            // Dev ID now fetched
+            break;
 
-		case TSL2591_DEV_ID_READY:
-			// Dev ID now fetched
-			break;
-
-		default:
-			break;
-	}
+        default:
+            break;
+    }
 }
 
 void initialize_Light()
 {
-	if (TSL2591_OK == tsl2591_initialise(tsl2591Callback))
-	{
-		// Driver initialized successfully
-		// Always check what tsl2591_initialise() returns
-	}
+    if (TSL2591_OK == tsl2591_initialise(tsl2591Callback))
+    {
+        // Driver initialized successfully
+        // Always check what tsl2591_initialise() returns
+    }
 }
 
 void lightTask_run()
 {
-	if (TSL2591_OK == tsl2591_enable())
-	{
-		// The power-up command is now sent to the sensor
-		// It can be powered down with a call to tsl2591_disable()
-	}
-	vTaskDelay(pdMS_TO_TICKS(6000));
+    if (TSL2591_OK == tsl2591_enable())
+    {
+        // The power-up command is now sent to the sensor
+        // It can be powered down with a call to tsl2591_disable()
+    }
+    vTaskDelay(pdMS_TO_TICKS(6000));
 
-	xSemaphoreTake(semaphoreLight, portMAX_DELAY);
+    xSemaphoreTake(semaphoreLight, portMAX_DELAY);
 
-	if (TSL2591_OK != tsl2591_fetchData())
-	{
-		// Something went wrong
-		// Investigate the return code further
-	}
-	else
-	{
-		// The light data will be ready after the driver calls the callback function with TSL2591_DATA_READY.
-	}
+    tsl2591_returnCode_t fetchDataStatus = tsl2591_fetchData();
 
-	xSemaphoreGive(semaphoreLight);
+    if (fetchDataStatus == TSL2591_OK)
+    {
+        printf("Light data fetched!\n");
+        // Process the fetched light data here
+    }
+    else if (fetchDataStatus == TSL2591_BUSY)
+    {
+        printf("Light sensor busy\n");
+    }
+    else if (fetchDataStatus == TSL2591_DRIVER_NOT_INITIALISED)
+    {
+        printf("Light driver not initialized\n");
+    }
+
+    xSemaphoreGive(semaphoreLight);
 }
 
 void lightTask_create()
 {
-	xTaskCreate(
-		_runLight,				       // Name of method
-		"Light",						   // A name just for humans
-		configMINIMAL_STACK_SIZE,	   // This stack size can be checked & adjusted by reading the Stack Highwater
-		NULL,					   	   // (void *pvParameters)
-		1,							   // the priority of the task
-		NULL						  
-// No TaskHandle created.
-);
+    xTaskCreate(
+        _runLight,                  // Name of method
+        "Light",                    // A name just for humans
+        configMINIMAL_STACK_SIZE,   // This stack size can be checked & adjusted by reading the Stack Highwater
+        NULL,                       // (void *pvParameters)
+        1,                          // the priority of the task
+        NULL
+    );
 }
 
 void _runLight(void* params)
 {
-	// In the future, we could use this for some INIT values
-	while (1) {
-		lightTask_run();
-	}
+    // In the future, we could use this for some INIT values
+    while (1)
+    {
+        lightTask_run();
+    }
 }
