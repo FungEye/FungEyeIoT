2d91347bcc0295f5ab3cf54e0e4958d17d13bb13
KarolisSad <92150126+KarolisSad@users.noreply.github.com>
2023-05-23 01:59:00 -0700

    Deleted: build files
    :+1:

--- build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/ninaw/OneDrive - ViaUC/Dokumenter/GitHub/FungEyeIoT
-# Build directory: C:/Users/ninaw/OneDrive - ViaUC/Dokumenter/GitHub/FungEyeIoT/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-include("C:/Users/ninaw/OneDrive - ViaUC/Dokumenter/GitHub/FungEyeIoT/build/appTest[1]_include.cmake")
-subdirs("_deps/googletest-build")
